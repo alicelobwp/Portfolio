fc8b84439bf1ec4b926b982c599c27796c21620c
alicelobwp <loboalicepereira@gmail.com>
2026-06-23 16:58:51 -0300

    fix name in the file

--- mariah-portfolio/src/app/page.tsx
+++ mariah-portfolio/src/app/page.tsx
@@ -4,11 +4,10 @@ import Top from "@/components/top";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
-          <Navbar/>
-      <Top/>
-  
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-seashell sm:items-start">
-      </main>
+      <Navbar />
+      <Top />
+
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-seashell sm:items-start"></main>
     </div>
   );
 }
